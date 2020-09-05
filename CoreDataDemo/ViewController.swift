d6757002754565adcb963a49d48f951a65fd8e66
Amani <amanikhunter@gmail.com>
2020-09-04 20:08:31 -0400

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Amani Hunter on 9/4/20.
+//  Copyright © 2020 INAMA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
